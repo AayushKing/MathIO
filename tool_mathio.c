036746e12f388b98df397413c26951ba69e6c6b3
AKaspal <111235859+AKaspal@users.noreply.github.com>
2024-11-15 00:41:58 -0500

    Update tool_mathio.c
    Help functionality and input validation

--- tool_mathio.c
+++ tool_mathio.c
@@ -1,25 +1,94 @@
-#include <mathio.h>
+#include "mathio.h"
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <ctype.h>
+
+/**
+ * Checks if a string represents a valid integer.
+ * @param str The string to check.
+ * @return 1 if valid, 0 otherwise.
+ */
+int is_valid_integer(const char *str) {
+    if (*str == '-' || *str == '+') str++;
+    if (!*str) return 0;
+    while (*str) {
+        if (!isdigit(*str)) return 0;
+        str++;
+    }
+    return 1;
+}
+
+void print_help() {
+    printf("MathIO - A simple command-line math tool.\n\n");
+    printf("Usage:\n");
+    printf("  mathio add <num1> <num2>\n");
+    printf("  mathio subtract <num1> <num2>\n");
+    printf("  mathio multiply <num1> <num2>\n");
+    printf("  mathio divide <num1> <num2>\n");
+    printf("  mathio print <message>\n");
+}
 
 int main(int argc, char *args[]) {
-    if (argc > 1) {
-        if (strcasecmp(args[1], "add") == 0 && args[2] && args[3]) {
-            printf("%d\n", add(atoi(args[2]), atoi(args[3])));
+    if (argc < 2) {
+        print_help();
+        return 1;
+    }
+
+    if (strcasecmp(args[1], "help") == 0 || strcasecmp(args[1], "--help") == 0 || strcasecmp(args[1], "-h") == 0) {
+        print_help();
+        return 0;
+    }
+
+    if (strcasecmp(args[1], "add") == 0 || 
+        strcasecmp(args[1], "subtract") == 0 || 
+        strcasecmp(args[1], "multiply") == 0 || 
+        strcasecmp(args[1], "divide") == 0) {
+        
+        if (argc < 4) {
+            fprintf(stderr, "Error: Insufficient arguments for '%s'.\n", args[1]);
+            print_help();
+            return 1;
         }
-        else if (strcasecmp(args[1], "subtract") == 0 && args[2] && args[3]) {
-            printf("%d\n", subtract(atoi(args[2]), atoi(args[3])));
+
+        if (!is_valid_integer(args[2]) || !is_valid_integer(args[3])) {
+            fprintf(stderr, "Error: Arguments must be valid integers.\n");
+            return 1;
         }
-        else if (strcasecmp(args[1], "multiply") == 0 && args[2] && args[3]) {
-            printf("%d\n", multiply(atoi(args[2]), atoi(args[3])));
+
+        int num1 = atoi(args[2]);
+        int num2 = atoi(args[3]);
+
+        if (strcasecmp(args[1], "add") == 0) {
+            printf("%d\n", add(num1, num2));
         }
-        else if (strcasecmp(args[1], "divide") == 0 && args[2] && args[3]) {
-            printf("%d\n", divide(atoi(args[2]), atoi(args[3])));
+        else if (strcasecmp(args[1], "subtract") == 0) {
+            printf("%d\n", subtract(num1, num2));
         }
-        else if (strcasecmp(args[1], "print") == 0 && args[2]) {
-            print(args[2]);
+        else if (strcasecmp(args[1], "multiply") == 0) {
+            printf("%d\n", multiply(num1, num2));
+        }
+        else if (strcasecmp(args[1], "divide") == 0) {
+            if (num2 == 0) {
+                fprintf(stderr, "Error: Division by zero is undefined.\n");
+                return 1;
+            }
+            printf("%d\n", divide(num1, num2));
         }
     }
+    else if (strcasecmp(args[1], "print") == 0) {
+        if (argc < 3) {
+            fprintf(stderr, "Error: Insufficient arguments for 'print'.\n");
+            print_help();
+            return 1;
+        }
+        print(args[2]);
+    }
+    else {
+        fprintf(stderr, "Error: Unknown command '%s'.\n", args[1]);
+        print_help();
+        return 1;
+    }
+
     return 0;
 }
